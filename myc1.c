e366b3c91d7bfc13f6b6c8b08d31ddd98b5c576f
Santunl <97426699+Santunl@users.noreply.github.com>
2022-02-27 12:43:45 +0530

    Create myc1.c

---
+++ myc1.c
@@ -0,0 +1,10 @@
+//https://allpcworld.comhttps://allpcworld.com/
+#include<stdio.h>
+void main()
+{
+    float a,b,area;
+    printf("Enater thre values\n");
+    scanf("%f%f",&a,&b);
+    printf("area=%0.2f",(1.0/2.0)*a*b);
+    
+}
